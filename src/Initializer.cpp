63236518a2e1735be800cbbba92715a31a8334e5
Sander Mertens <sander.mertens8@gmail.com>
2018-03-07 00:59:28 -0800

    Fix compiler errors due to renaming parts of AST

--- src/Initializer.cpp
+++ src/Initializer.cpp
@@ -6,10 +6,10 @@ int16_t ast_Initializer_populateHelper(
     ast_Initializer _this,
     ast_InitializerHelper helper)
 {
-    corto_iter it = corto_ll_iter(_this->args);
+    corto_iter it = corto_ll_iter(_this->values);
 
     while (corto_iter_hasNext(&it)) {
-        ast_Argument arg = (ast_Argument)corto_iter_next(&it);
+        ast_InitializerValue arg = (ast_InitializerValue)corto_iter_next(&it);
 
         if (arg->key) {
             if (ast_InitializerHelper_member(helper, arg->key)) {
@@ -17,12 +17,12 @@ int16_t ast_Initializer_populateHelper(
             }
         }
 
-        if (corto_instanceof(ast_Initializer_o, arg->expr)) {
+        if (corto_instanceof(ast_Initializer_o, arg->value)) {
             if (ast_InitializerHelper_push(helper)) {
                 goto error;
             }
 
-            if (safe_ast_Initializer_populateHelper(arg->expr, helper)) {
+            if (safe_ast_Initializer_populateHelper(arg->value, helper)) {
                 goto error;
             }
 
@@ -30,10 +30,10 @@ int16_t ast_Initializer_populateHelper(
                 goto error;
             }
         } else {
-            ast_Node data = safe_ast_Statement_getData(arg->expr);
+            ast_Node data = safe_ast_Statement_getData(arg->value);
             if (!data) {
                 corto_throw("%s unexpected in initializer",
-                  corto_idof(corto_typeof(arg->expr)));
+                  corto_idof(corto_typeof(arg->value)));
                 goto error;
             }
 
@@ -43,7 +43,7 @@ int16_t ast_Initializer_populateHelper(
                 }
             } else {
                 corto_throw("invalid %s element in initializer",
-                  corto_idof(corto_typeof(arg->expr)));
+                  corto_idof(corto_typeof(arg->value)));
                 goto error;
             }
         }
@@ -58,11 +58,11 @@ int16_t ast_Initializer_visit_v(
     ast_Initializer _this,
     ast_Visitor visitor)
 {
-    corto_iter it = corto_ll_iter(_this->args);
+    corto_iter it = corto_ll_iter(_this->values);
 
     while (corto_iter_hasNext(&it)) {
-        ast_Argument arg = (ast_Argument)corto_iter_next(&it);
-        if (safe_ast_Visitor_visit(visitor, arg->expr)) {
+        ast_InitializerValue arg = (ast_InitializerValue)corto_iter_next(&it);
+        if (safe_ast_Visitor_visit(visitor, arg->value)) {
             goto error;
         }
     }
